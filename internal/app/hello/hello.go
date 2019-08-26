f5b051d8a059ee6234c5ed10fd5e03cad41b221c
Alik Khilazhev <alikhil@mail.ru>
2019-08-26 18:51:08 +0300

    Small refactor

--- internal/app/hello/hello.go
+++ internal/app/hello/hello.go
@@ -7,17 +7,17 @@ import (
 // HelloHandler - simple http handler
 func (ctx *Context) HelloHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("hello!"))
+	w.Write([]byte("hi!"))
 }
 
 // ReadinessHealthcheckHandler - dumb healthcheck method
 func (ctx *Context) ReadinessHealthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("hello!"))
+	w.Write([]byte("ok!"))
 }
 
 // LivenessHealthcheckHandler - dumb healthcheck method
 func (ctx *Context) LivenessHealthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("hello!"))
+	w.Write([]byte("ok!"))
 }
